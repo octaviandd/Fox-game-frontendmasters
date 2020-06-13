ba0ceea4510c421963528952107d5dc4b0f2c6b7
octaviandd <octaviandd@yahoo.com>
2020-06-13 22:00:42 +0100

    first commit

---
+++ src/gameState.js
@@ -0,0 +1,13 @@
+const gameState = {
+  current: "INIT",
+  clock: 1,
+  tick(){
+    this.clock++
+    console.log('clock:',this.clock)
+    return this.clock;
+  },
+  
+}
+
+
+export default gameState
